683ea7e2976617dd36309ed50ace2a09927b5331
rahulsingh178 <rahulsingh178@gmail.com>
2024-10-01 12:58:26 +0200

    Updated main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { evaluate } from 'mathjs';
+// import { evaluate } from 'mathjs';
 
 
-  export function evaluateFromMain(formula: any ) : number  {
+  export function evaluate(formula: any ) : number  {
 
 //Scanner
 // Define token types
@@ -9,10 +9,10 @@ enum TokenType {
     NUMBER,
     PLUS,
     MINUS,
-    MUL,
-    DIV,
-    LPAREN,
-    RPAREN,
+    MULTIPLY,
+    DIVISION,
+    LPARENTHESIS,
+    RPARENTHESIS,
     IDENTIFIER,
     EOF,
   }
@@ -86,16 +86,16 @@ enum TokenType {
             return new Token(TokenType.MINUS, '-');
           case '*':
             this.advance();
-            return new Token(TokenType.MUL, '*');
+            return new Token(TokenType.MULTIPLY, '*');
           case '/':
             this.advance();
-            return new Token(TokenType.DIV, '/');
+            return new Token(TokenType.DIVISION, '/');
           case '(':
             this.advance();
-            return new Token(TokenType.LPAREN, '(');
+            return new Token(TokenType.LPARENTHESIS, '(');
           case ')':
             this.advance();
-            return new Token(TokenType.RPAREN, ')');
+            return new Token(TokenType.RPARENTHESIS, ')');
           default:
             throw new Error(`Unknown character: ${this.currentChar}`);
         }
@@ -105,7 +105,9 @@ enum TokenType {
     }
   }
 
-  abstract class ASTNode {}
+
+//Parser structures 
+abstract class ASTNode {}
 
 class NumberNode extends ASTNode {
   constructor(public value: number) {
@@ -113,13 +115,13 @@ class NumberNode extends ASTNode {
   }
 }
 
-class BinOpNode extends ASTNode {
+class BinaryOperationNode extends ASTNode {
   constructor(public left: ASTNode, public op: Token, public right: ASTNode) {
     super();
   }
 }
 
-class UnaryOpNode extends ASTNode {
+class NegationNode extends ASTNode {
   constructor(public op: Token, public node: ASTNode) {
     super();
   }
@@ -138,7 +140,7 @@ class Parser {
       this.currentToken = this.scanner.getNextToken();
     }
   
-    private eat(type: TokenType) {
+    private checkTokenTypeAndGetNextToken(type: TokenType) {
       if (this.currentToken.type === type) {
         this.currentToken = this.scanner.getNextToken();
       } else {
@@ -151,23 +153,23 @@ class Parser {
       const token = this.currentToken;
   
       if (token.type === TokenType.NUMBER) {
-        this.eat(TokenType.NUMBER);
+        this.checkTokenTypeAndGetNextToken(TokenType.NUMBER);
         return new NumberNode(parseFloat(token.value));
-      } else if (token.type === TokenType.LPAREN) {
-        this.eat(TokenType.LPAREN);
+      } else if (token.type === TokenType.LPARENTHESIS) {
+        this.checkTokenTypeAndGetNextToken(TokenType.LPARENTHESIS);
         const node = this.expression();
-        this.eat(TokenType.RPAREN);
+        this.checkTokenTypeAndGetNextToken(TokenType.RPARENTHESIS);
         return node;
       } else if (token.type === TokenType.IDENTIFIER) {
         const funcName = token.value;
-        this.eat(TokenType.IDENTIFIER);
-        this.eat(TokenType.LPAREN);
+        this.checkTokenTypeAndGetNextToken(TokenType.IDENTIFIER);
+        this.checkTokenTypeAndGetNextToken(TokenType.LPARENTHESIS);
         const arg = this.expression();
-        this.eat(TokenType.RPAREN);
+        this.checkTokenTypeAndGetNextToken(TokenType.RPARENTHESIS);
         return new FunctionCallNode(funcName, arg);
       } else if (token.type === TokenType.MINUS) {
-        this.eat(TokenType.MINUS);
-        return new UnaryOpNode(token, this.factor());
+        this.checkTokenTypeAndGetNextToken(TokenType.MINUS);
+        return new NegationNode(token, this.factor());
       }
   
       throw new Error('Invalid factor');
@@ -177,14 +179,14 @@ class Parser {
     private term(): ASTNode {
       let node = this.factor();
   
-      while (this.currentToken.type === TokenType.MUL || this.currentToken.type === TokenType.DIV) {
+      while (this.currentToken.type === TokenType.MULTIPLY || this.currentToken.type === TokenType.DIVISION) {
         const token = this.currentToken;
-        if (token.type === TokenType.MUL) {
-          this.eat(TokenType.MUL);
-        } else if (token.type === TokenType.DIV) {
-          this.eat(TokenType.DIV);
+        if (token.type === TokenType.MULTIPLY) {
+          this.checkTokenTypeAndGetNextToken(TokenType.MULTIPLY);
+        } else if (token.type === TokenType.DIVISION) {
+          this.checkTokenTypeAndGetNextToken(TokenType.DIVISION);
         }
-        node = new BinOpNode(node, token, this.factor());
+        node = new BinaryOperationNode(node, token, this.factor());
       }
   
       return node;
@@ -197,11 +199,11 @@ class Parser {
       while (this.currentToken.type === TokenType.PLUS || this.currentToken.type === TokenType.MINUS) {
         const token = this.currentToken;
         if (token.type === TokenType.PLUS) {
-          this.eat(TokenType.PLUS);
+          this.checkTokenTypeAndGetNextToken(TokenType.PLUS);
         } else if (token.type === TokenType.MINUS) {
-          this.eat(TokenType.MINUS);
+          this.checkTokenTypeAndGetNextToken(TokenType.MINUS);
         }
-        node = new BinOpNode(node, token, this.term());
+        node = new BinaryOperationNode(node, token, this.term());
       }
   
       return node;
@@ -218,7 +220,7 @@ class Parser {
         return node.value;
       }
   
-      if (node instanceof BinOpNode) {
+      if (node instanceof BinaryOperationNode) {
         const leftVal = this.visit(node.left);
         const rightVal = this.visit(node.right);
   
@@ -227,14 +229,17 @@ class Parser {
             return leftVal + rightVal;
           case TokenType.MINUS:
             return leftVal - rightVal;
-          case TokenType.MUL:
+          case TokenType.MULTIPLY:
             return leftVal * rightVal;
-          case TokenType.DIV:
+          case TokenType.DIVISION:
+            if (rightVal == 0){
+                throw new Error('Trying to divide by zero');
+            }
             return leftVal / rightVal;
         }
       }
   
-      if (node instanceof UnaryOpNode) {
+      if (node instanceof NegationNode) {
         const value = this.visit(node.node);
         return -value;
       }
@@ -251,7 +256,7 @@ class Parser {
           case 'sqrt':
             return Math.sqrt(argValue);
           case 'pow':
-            return Math.pow(argValue, 2); // Example: hard-coded exponent 2
+            return Math.pow(argValue, 2); // Example: hard-coded exponent 2. Ideal : pow(2,3) = 8 
         }
       }
   
